5435f569426dbf31b99439b7784c4a5e4d1a8e65
M Ehsan Ahmad <mehsanahmad9290@gmail.com>
2026-08-14 05:58:30 -0700

    add lecture no. 03

---
+++ lecture 03/index.js
@@ -0,0 +1,12 @@
+let score = null;
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+let checkTypeOfNumer = Number(score);
+
+console.log(typeof checkTypeOfNumer);
+console.log(checkTypeOfNumer);
+
+
