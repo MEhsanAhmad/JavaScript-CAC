78ba539e4def447d095760b0466509faab411630
M Ehsan Ahmad <mehsanahmad9290@gmail.com>
2026-08-14 05:41:48 -0700

    add lecture no. 02

--- Lecture 01/index.js
+++ Lecture 01/index.js
@@ -1,1 +1,28 @@
 console.log("Muhammad Ehsan Ahmad");
+const name = "Muhammd Ehsan Ahmad";
+const accountID = 12415445;
+let accountEmail = "MuhammadEhsanAhmad@gmail.com";
+var userName = "Muhammad Ehsan Ahmad"; // var not used because of scope (block & functions)
+
+city = "Lahore";
+
+console.log(name);
+console.log(accountID);
+console.log(accountEmail);
+
+accountEmail = "MEhsanAhmad@gmail.com";
+
+console.log(city);
+console.log(accountEmail);
+
+// name = "ARslan"    const not change 
+console.log(name);
+
+userName = "Arslan";
+console.log(userName);
+
+console.log();
+
+
+
+console.table[accountID, accountEmail, userName]; 
---
+++ lecture 02/index.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all code like new code 
+
+// alert(3 + 3)  //we are using node js, browser
+
+console.log(3);
+
+ 
